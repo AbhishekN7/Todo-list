15cb89d48c4afee7a881fd1fd1ae056b6c9c6066
Abhishek Narkhede <abhisheknarkhede123@gmail.com>
2021-09-25 17:12:52 +0530

    Updates

---
+++ precache-manifest.5618e3d819d836e2b8bb328c85c09d04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8389df1184259edfe4b5e053bbe0fd8",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/Todo-list/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "1f9f3174ceeae706517d",
+    "url": "/Todo-list/static/js/main.8ab7a2a3.chunk.js"
+  },
+  {
+    "revision": "005b816af9930894bc69",
+    "url": "/Todo-list/static/js/runtime-main.ab65f0cb.js"
+  }
+]);
